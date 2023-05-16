112ff2ba5b47d599de877cf2ca6df8d8fff82a5d
victorandujar <victorandujarmurcia@gmail.com>
2023-05-16 12:13:56 +0200

    Create usersSlice and its loginUser action

---
+++ src/store/features/usersSlice/usersSlice.ts
@@ -0,0 +1,29 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { UserState, UserStructure } from "./types/types";
+
+const initialState: UserState = {
+  email: "",
+  id: "",
+  token: "",
+  isLogged: false,
+};
+
+const usersSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginUser: (
+      currentUserState,
+      action: PayloadAction<UserStructure>
+    ): UserState => ({
+      ...currentUserState,
+      email: action.payload.email,
+      id: action.payload.id,
+      token: action.payload.token,
+      isLogged: true,
+    }),
+  },
+});
+
+export const { loginUser: loginUserActionCreator } = usersSlice.actions;
+export const userReducer = usersSlice.reducer;
