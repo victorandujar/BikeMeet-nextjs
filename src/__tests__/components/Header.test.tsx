4399b51175fe493b6a6b4bdbb25e54a93988560d
victorandujar <victorandujarmurcia@gmail.com>
2023-05-17 11:57:03 +0200

    Modify testing because of adding ThemeProvider to the project

--- src/__tests__/components/Header.test.tsx
+++ src/__tests__/components/Header.test.tsx
@@ -1,28 +1,36 @@
 import Header from "@/components/Header/Header";
+import { store } from "@/store/store";
+import theme from "@/styles/Theme";
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
 
 describe("Given a Header component", () => {
   describe("When it is rendered", () => {
     test("Then it should show an image with the logo", () => {
-      render(<Header />);
+      render(
+        <ThemeProvider theme={theme}>
+          <Provider store={store}>
+            <Header />
+          </Provider>
+        </ThemeProvider>
+      );
 
       const expectedLogo = screen.getByRole("img");
 
       expect(expectedLogo).toBeInTheDocument();
     });
 
-    test("Then it should show a nav bar", () => {
-      render(<Header />);
-
-      const expectedNavBar = screen.getByRole("navigation");
-
-      expect(expectedNavBar).toBeInTheDocument();
-    });
-
     test("Then it should show a heading with the text 'BikeMeet'", () => {
-      const headingText = "BikeMeet";
+      const headingText = "Bike";
 
-      render(<Header />);
+      render(
+        <ThemeProvider theme={theme}>
+          <Provider store={store}>
+            <Header />
+          </Provider>
+        </ThemeProvider>
+      );
 
       const expectedHeader = screen.getByRole("heading", { name: headingText });
 
