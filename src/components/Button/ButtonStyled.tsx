98981ab6dfd13bb74816c884ad60550231cd18c2
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-05-22 11:02:14 +0200

    Fix styles from Button component and add styled to it

--- src/components/Button/ButtonStyled.tsx
+++ src/components/Button/ButtonStyled.tsx
@@ -1,15 +1,13 @@
 import styled from "styled-components";
 
 const ButtonStyled = styled.button`
-  .btn-outline-success {
-    border: 1px solid black;
-    color: ${(props) => props.theme.colors.mainColorText};
-    font-size: 12px;
+  border: 1px solid black;
+  color: ${(props) => props.theme.colors.mainColorText};
+  font-size: 12px;
 
-    :hover {
-      background-color: ${(props) => props.theme.colors.secondaryColorButton};
-      color: ${(props) => props.theme.colors.secondaryColorText};
-    }
+  :hover {
+    background-color: ${(props) => props.theme.colors.secondaryColorButton};
+    color: ${(props) => props.theme.colors.secondaryColorText};
   }
 `;
 
