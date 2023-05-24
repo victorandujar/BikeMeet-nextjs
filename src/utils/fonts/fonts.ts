cf185bfe6d216c7f7d0e281ee43894f874a9bd25
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-05-24 11:06:31 +0200

    Refactor project fonts to a single file at utils

---
+++ src/utils/fonts/fonts.ts
@@ -0,0 +1,4 @@
+import { Odibee_Sans, Poppins } from "next/font/google";
+
+export const primaryFont = Poppins({ subsets: ["latin"], weight: "400" });
+export const secondaryFont = Odibee_Sans({ subsets: ["latin"], weight: "400" });
