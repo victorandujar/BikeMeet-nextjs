42307b78922e8658f53a3445887c8877ed38768f
victorandujar <victorandujarmurcia@gmail.com>
2023-05-27 12:17:58 +0200

    Add testing for RegisterForm functionality

--- src/__tests__/components/RegisterForm.test.tsx
+++ src/__tests__/components/RegisterForm.test.tsx
@@ -1,6 +1,25 @@
 import RegisterForm from "@/components/RegisterForm/RegisterForm";
+import {
+  mockUserRegisterCredentials,
+  mockUserRegisterCredentialsForm,
+} from "@/mocks/userMocks/userMocks";
 import renderWithProviders from "@/utils/testUtils/testUtils";
-import { screen } from "@testing-library/react";
+import { act, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+const mockedUseRouter = {
+  push: jest.fn(),
+};
+
+jest.mock("next/navigation", () => ({
+  ...jest.requireActual("next/navigation"),
+  useRouter: () => mockedUseRouter,
+}));
+
+const mockRegisterUser = jest.fn();
+jest.mock("../../hooks/useUser/useUser", () => () => ({
+  registerUser: mockRegisterUser,
+}));
 
 describe("Given a RegisterForm component", () => {
   describe("When it is rendered", () => {
@@ -22,6 +41,133 @@ describe("Given a RegisterForm component", () => {
       const expectedButton = screen.getByRole("button", { name: buttonText });
 
       expect(expectedButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user writes in the form inputs", () => {
+    test("Then it should change the state of each input", async () => {
+      const nameLabel = "Name";
+      const surnameLabel = "Surname";
+      const usernameLabel = "Username";
+      const emailLabel = "Email adress";
+      const passwordLabel = "Password";
+      const repeatPasswordLabel = "Repeat password";
+
+      const nameText = mockUserRegisterCredentialsForm.name;
+      const surnameText = mockUserRegisterCredentialsForm.surname;
+      const usernameText = mockUserRegisterCredentialsForm.username;
+      const emailText = mockUserRegisterCredentialsForm.email;
+      const passwordText = mockUserRegisterCredentialsForm.password;
+      const repeatPasswordText = mockUserRegisterCredentialsForm.repeat;
+
+      renderWithProviders(<RegisterForm />);
+
+      const nameInput = screen.getByLabelText(nameLabel);
+      const surnameInput = screen.getByLabelText(surnameLabel);
+      const usernameInput = screen.getByLabelText(usernameLabel);
+      const emailInput = screen.getByLabelText(emailLabel);
+      const passwordInput = screen.getByLabelText(passwordLabel);
+      const repeatPasswordInput = screen.getByLabelText(repeatPasswordLabel);
+
+      await act(async () => await userEvent.type(nameInput, nameText));
+      await act(async () => await userEvent.type(surnameInput, surnameText));
+      await act(async () => await userEvent.type(usernameInput, usernameText));
+      await act(async () => await userEvent.type(emailInput, emailText));
+      await act(async () => await userEvent.type(passwordInput, passwordText));
+      await act(
+        async () =>
+          await userEvent.type(repeatPasswordInput, repeatPasswordText)
+      );
+
+      expect(nameInput).toHaveValue(nameText);
+      expect(surnameInput).toHaveValue(surnameText);
+      expect(usernameInput).toHaveValue(usernameText);
+      expect(emailInput).toHaveValue(emailText);
+      expect(passwordInput).toHaveValue(passwordText);
+      expect(repeatPasswordInput).toHaveValue(repeatPasswordText);
+    });
+  });
+
+  describe("When the user submits the form information to register", () => {
+    test("Then the registerUser function has to be called", async () => {
+      const namePlaceholderText = "Enter your first name";
+      const surnamePlaceholderText = "Enter your surname";
+      const usernamePlaceholderText = "Create a username";
+      const emailPlaceHolderText = "Enter a valid email";
+      const passwordPlaceholderText = "Password. 10 characters";
+      const repeatPasswordPlaceholderText = "Repeat password";
+      const buttonText = "Join us!";
+
+      renderWithProviders(<RegisterForm />);
+
+      const expectedNameInput =
+        screen.getByPlaceholderText(namePlaceholderText);
+      const expectedSurnameInput = screen.getByPlaceholderText(
+        surnamePlaceholderText
+      );
+      const expectedUsernameInput = screen.getByPlaceholderText(
+        usernamePlaceholderText
+      );
+      const expectedEmailInput =
+        screen.getByPlaceholderText(emailPlaceHolderText);
+      const expectedPasswordInput = screen.getByPlaceholderText(
+        passwordPlaceholderText
+      );
+      const expectedRepeatPassswordInput = screen.getByPlaceholderText(
+        repeatPasswordPlaceholderText
+      );
+      const expectedSubmitButton = screen.getByRole("button", {
+        name: buttonText,
+      });
+
+      await act(
+        async () =>
+          await userEvent.type(
+            expectedNameInput,
+            mockUserRegisterCredentialsForm.name
+          )
+      );
+      await act(
+        async () =>
+          await userEvent.type(
+            expectedSurnameInput,
+            mockUserRegisterCredentialsForm.surname
+          )
+      );
+      await act(
+        async () =>
+          await userEvent.type(
+            expectedUsernameInput,
+            mockUserRegisterCredentialsForm.username
+          )
+      );
+      await act(
+        async () =>
+          await userEvent.type(
+            expectedEmailInput,
+            mockUserRegisterCredentialsForm.email
+          )
+      );
+      await act(
+        async () =>
+          await userEvent.type(
+            expectedPasswordInput,
+            mockUserRegisterCredentialsForm.password
+          )
+      );
+      await act(
+        async () =>
+          await userEvent.type(
+            expectedRepeatPassswordInput,
+            mockUserRegisterCredentialsForm.repeat
+          )
+      );
+
+      await act(async () => await userEvent.click(expectedSubmitButton));
+
+      expect(mockRegisterUser).toHaveBeenCalledWith(
+        mockUserRegisterCredentials
+      );
     });
   });
 });
