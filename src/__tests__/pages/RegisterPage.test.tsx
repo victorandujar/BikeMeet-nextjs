60487fde611bf646987a316cf0ddb15ff32c4669
victorandujar <victorandujarmurcia@gmail.com>
2023-05-27 12:19:11 +0200

    Fix tests because adding the method push from router to RegisterForm

--- src/__tests__/pages/RegisterPage.test.tsx
+++ src/__tests__/pages/RegisterPage.test.tsx
@@ -2,6 +2,15 @@ import SignUpPage from "@/pages/sign-up";
 import renderWithProviders from "@/utils/testUtils/testUtils";
 import { screen } from "@testing-library/react";
 
+const mockedUseRouter = {
+  push: jest.fn(),
+};
+
+jest.mock("next/navigation", () => ({
+  ...jest.requireActual("next/navigation"),
+  useRouter: () => mockedUseRouter,
+}));
+
 describe("Given a RegisterPage component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a heading with the text 'Join our community!'", () => {
