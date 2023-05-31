5ec3f49a442323aecf908d7a65ab298ef6718a6a
victorandujar <victorandujarmurcia@gmail.com>
2023-05-31 15:18:57 +0200

    Modify pages folder structure

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,7 +4,6 @@ import { rest } from "msw";
 export const handlers = [
   rest.post(
     `${process.env.NEXT_PUBLIC_API_URL}${userEndpoints.users}${userEndpoints.login}`,
-    async (req, res, ctx) =>
-      res(ctx.status(200), ctx.json({ token: "vik27634fvj" }))
+    (req, res, ctx) => res(ctx.status(200), ctx.json({ token: "vik27634fvj" }))
   ),
 ];
