545e44bd776a44e34fb0cdd7f69fb3db8bfa036c
victorandujar <victorandujarmurcia@gmail.com>
2023-06-04 11:47:35 +0200

    Modify LoginForm testing because of adding modals functionality

--- src/mocks/handlers.ts
+++
@@ -1,9 +0,0 @@
-import userEndpoints from "@/utils/userEndpoints/userEndpoints";
-import { rest } from "msw";
-
-export const handlers = [
-  rest.post(
-    `${process.env.NEXT_PUBLIC_API_URL}${userEndpoints.users}${userEndpoints.login}`,
-    (req, res, ctx) => res(ctx.status(200), ctx.json({ token: "vik27634fvj" }))
-  ),
-];
