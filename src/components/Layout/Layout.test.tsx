58938d1d8c4630ea9ed294fc408f60a89da20a4d
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-06-09 17:39:35 +0200

    Add testing for Layout component

---
+++ src/components/Layout/Layout.test.tsx
@@ -0,0 +1,25 @@
+import renderWithProviders from "@/utils/testUtils/testUtils";
+import Layout from "./Layout";
+import { screen } from "@testing-library/react";
+
+const mockedUsedRouter = jest.fn();
+jest.mock("next/router", () => ({
+  ...jest.requireActual("next/router"),
+  useRouter: () => mockedUsedRouter,
+}));
+
+describe("Given a Layout component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a heading with the text 'Bike'", () => {
+      const headingText = "Bike";
+
+      renderWithProviders(<Layout />);
+
+      const expectedHeading = screen.getByRole("heading", {
+        name: headingText,
+      });
+
+      expect(expectedHeading).toBeInTheDocument();
+    });
+  });
+});
