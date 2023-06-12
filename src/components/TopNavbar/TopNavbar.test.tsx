2659823bedd23f37a5842c4a34503365d15a735c
Marc Girbau <marc.girbau94@gmail.com>
2023-06-12 13:07:13 +0200

    Wrapp tests in SessionProvider

--- src/components/TopNavbar/TopNavbar.test.tsx
+++ src/components/TopNavbar/TopNavbar.test.tsx
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import TopNavbar from "./TopNavbar";
+import renderWithProviders from "@/utils/testUtils/testUtils";
 
 describe("Given a TopNavbar component", () => {
   describe("When rendered", () => {
     test("Then it should show a text with the labels `Home`, `Nearby`, `Create`, `Favourites` and `Profile`", () => {
       const labels = ["Home", "Nearby", "Create", "Profile", "Favourites"];
 
-      render(<TopNavbar />);
+      renderWithProviders(<TopNavbar />);
 
       labels.forEach((label) => {
         const labelElement = screen.getByText(label);
