2e330ddfd6ce217c4b38b2789ead93b0cefe39aa
Marc Girbau <marc.girbau94@gmail.com>
2023-06-14 13:20:13 +0200

    Test useToken custom hook

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,15 @@
+import useToken from "@/hooks/useToken/useToken";
 import BottomNavbarWrapper from "../BottomNavbarWrapper/BottomNavbarWrapper";
 import Header from "../Header/Header";
+import { useEffect } from "react";
 
 const Layout = (): JSX.Element => {
+  const { getToken } = useToken();
+
+  useEffect(() => {
+    getToken();
+  }, [getToken]);
+
   return (
     <>
       <Header />
