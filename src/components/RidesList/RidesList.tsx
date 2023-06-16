a05b3816f724e626a6ca3e308b58b424051900a4
Róman Demchuk <roman.demchuk@gmail.com>
2023-06-16 18:56:28 +0200

    Add RidesList component

---
+++ src/components/RidesList/RidesList.tsx
@@ -0,0 +1,21 @@
+import { RideStructureData } from "../Ride/types";
+import Ride from "../Ride/Ride";
+import RidesListStyled from "./RidesListStyled";
+
+interface RidesListProps {
+  rides: RideStructureData[];
+}
+
+const RidesList = ({ rides }: RidesListProps): JSX.Element => {
+  return (
+    <RidesListStyled>
+      {rides.map((ride) => (
+        <li key={ride.id} className="list__item">
+          <Ride ride={ride} />
+        </li>
+      ))}
+    </RidesListStyled>
+  );
+};
+
+export default RidesList;
