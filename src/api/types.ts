1a41b571ba9668966175aedd9444cc4fdaeacfa0
Marc Girbau <marc.girbau94@gmail.com>
2023-06-18 11:23:39 +0200

    Create Dashboard page

---
+++ src/api/types.ts
@@ -0,0 +1,5 @@
+import { RideStructureData } from "@/components/Ride/types";
+
+export interface getRidesApiResponse {
+  rides: RideStructureData[];
+}
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,21 @@
 import { NextPage } from "next";
 import RidesList from "@/components/RidesList/RidesList";
+import useRides from "@/hooks/useRides/useRides";
+import { useQuery } from "react-query";
+import { getRidesApiResponse } from "@/api/types";
 
 const Dashboard: NextPage = (): JSX.Element => {
-  return <></>;
+  const { getRides } = useRides();
+
+  const result = useQuery("rides", getRides);
+
+  return (
+    <>
+      {!result.isLoading && (
+        <RidesList rides={(result.data! as getRidesApiResponse).rides} />
+      )}
+    </>
+  );
 };
 
 export default Dashboard;
