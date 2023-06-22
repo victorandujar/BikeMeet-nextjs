3b7787a65b5777e4c255b87bc48418b85012fdd1
Róman Demchuk <roman.demchuk@gmail.com>
2023-06-22 16:09:51 +0200

    Style RidesList component

--- src/components/RidesList/RidesListStyled.ts
+++ src/components/RidesList/RidesListStyled.ts
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
 const RidesListStyled = styled.ul`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 20px 0 20px 0;
-  gap: 40px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 20px;
+  justify-items: center;
 `;
 
 export default RidesListStyled;
