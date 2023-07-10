1d7ebdf91a30eaa2e85a7fc68adf1d63fe9a59ef
victorandujar <victorandujarmurcia@gmail.com>
2023-07-10 11:46:36 +0200

    Create ProfileDropDownNavbar component to manage app user actions

---
+++ src/components/ProfileDropDownNavbar/ProfileDropDownNavbar.tsx
@@ -0,0 +1,121 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import Button from "../Button/Button";
+import { signOut } from "next-auth/react";
+import endpoints from "@/utils/endpoints/endpoints";
+import { useAppSelector } from "@/store/hooks";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+const ProfileDropDownNavbar = (): React.ReactElement => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const { user } = useAppSelector((state) => state.user);
+
+  return (
+    <React.Fragment>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? "account-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+          >
+            <Avatar sx={{ width: 32, height: 32 }} src={user.image}>
+              {user.name.charAt(0)}
+            </Avatar>
+            <ArrowDropDownIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            mt: 1.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+            },
+            display: "flex",
+            flexDirection: "column",
+            gap: "30px",
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Avatar src={user.image} /> Profile
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          <Button
+            text="Log out"
+            actionOnClick={() =>
+              signOut({
+                redirect: true,
+                callbackUrl: endpoints.login,
+              })
+            }
+            isDisabled={false}
+          />
+        </MenuItem>
+      </Menu>
+    </React.Fragment>
+  );
+};
+
+export default ProfileDropDownNavbar;
