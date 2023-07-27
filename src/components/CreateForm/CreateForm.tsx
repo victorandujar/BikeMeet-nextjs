df7b758f9d96f1e6b78b00cca6df293beefe5b55
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-07-27 17:26:33 +0200

    Add surfaceTypes file to manage surface type at CreateForm

--- src/components/CreateForm/CreateForm.tsx
+++ src/components/CreateForm/CreateForm.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { InputLabel, MenuItem, Select, Typography } from "@mui/material";
+import { MenuItem, Select, Typography } from "@mui/material";
 import CreateFormStyled from "./CreateFormStyled";
 import PlacesLocation from "../PlacesLocation/PlacesLocation";
 import surfaceTypes from "@/utils/surfaceTypes/surfaceTypes";
--- src/components/PlacesLocation/PlacesLocation.tsx
+++ src/components/PlacesLocation/PlacesLocation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { StandaloneSearchBox, LoadScriptNext } from "@react-google-maps/api";
 import TextField from "@mui/material/TextField";
 
